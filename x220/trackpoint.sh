6ac52a4f7fb4bfa7c3bb15a20b0ac041969a85ea
pankid <schoonover.nicholsa@gmail.com>
2019-10-18 11:58:33 -0500

    added proton bridge start in xinit

--- x220/trackpoint.sh
+++ x220/trackpoint.sh
@@ -1,19 +1,10 @@
-dbus-update-activation-environment --systemd DISPLAY
-eval $(/usr/bin/gnome-keyring-daemon --start --components=pkcs11,secrets,ssh)
-export SSH_AUTH_SOCK
-xrdb -merge ~/.Xresources&
-redshift -l 36.1:-86.8 -t 6500K:2200K&
-nextcloud&
-pasystray&
+#!/bin/bash
+## Set the kensington trackball to have accelleration and slow down the velocity. 
+#
 
-
-##TRACKPOINTSETTINGS
 #for the vector, the last number decides how fast the velocity is. the higher the number the slower it will be
 xinput set-prop 'TPPS/2 IBM TrackPoint' 'Coordinate Transformation Matrix' 1, 0, 0, 0, 1, 0, 0, 0, 8
 
 #this can be a value between -1 and +1. higher is more accelleration. 
 xinput set-prop 'TPPS/2 IBM TrackPoint'  'libinput Accel Speed' .2
 
-protonmail-bridge &
-
-exec i3 
